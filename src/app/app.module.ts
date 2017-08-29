901172f8114a9a2889b750a3e54fdcd20db24bf4
Rajat Garg <RajatGarg@fico.com>
2017-08-29 19:21:38 +0530

    Update selection array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
+import { FileService } from './services/file.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ListFilesComponent } from './components/list-files/list-files.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { routing } from './app.routing';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { RenameComponent } from './components/rename/rename.component';
+import { DeleteComponent } from './components/delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListFilesComponent,
+    SidebarComponent,
+    ToolbarComponent,
+    RenameComponent,
+    DeleteComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot(),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [FileService, NgbActiveModal],
+  bootstrap: [AppComponent],
+  entryComponents: [RenameComponent, DeleteComponent]
 })
 export class AppModule { }
